db88aa8f68393cab2c49134f4fcd88ba8ccf5ee3
99455 <99455@LAPTOP-6F6HSN45>
2023-03-19 11:30:50 +0400

    Add project files.

---
+++ ConsoleApp1task5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
